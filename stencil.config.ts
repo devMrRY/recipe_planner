86d51b327ba7d48596f7e7cb231f440e53deea32
Rahul Yadav <rahul.yadav06@nagarro.com>
2026-08-30 02:48:56 +0530

    fix: removed duplicate loader

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,12 +2,17 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'recipe-components',
+
   outputTargets: [
-    { type: 'dist' },
-    { type: 'dist-custom-elements' },
-    { type: 'www', serviceWorker: null }
+    {
+      type: 'dist',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
   ],
+
   extras: {
-    scriptDataOpts: true
-  }
+    scriptDataOpts: true,
+  },
 };
